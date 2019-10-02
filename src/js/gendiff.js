01e1c24aeb47f3cbe5441caebaac93bc975657e5
Роман <kromanjs@gmail.com>
2019-10-02 20:09:36 +0300

    remove duplication

--- src/js/gendiff.js
+++ src/js/gendiff.js
@@ -5,28 +5,36 @@ const propertyActions = [
   {
     name: 'unchanged',
     check: (obj1, obj2, key) => has(obj1, key) && has(obj2, key) && obj1[key] === obj2[key],
-    result: (obj1, obj2, key) => `    ${key}: ${obj1[key]}`,
+    toString: (obj1, obj2, key) => `    ${key}: ${obj1[key]}`,
   },
   {
     name: 'changedValue',
     check: (obj1, obj2, key) => has(obj1, key) && has(obj2, key) && obj1[key] !== obj2[key],
-    result: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`,
+    toString: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`,
   },
   {
     name: 'deletedValue',
     check: (obj1, obj2, key) => has(obj1, key) && !has(obj2, key),
-    result: (obj1, obj2, key) => `  - ${key}: ${obj1[key]}`,
+    toString: (obj1, obj2, key) => `  - ${key}: ${obj1[key]}`,
   },
   {
     name: 'addedValue',
     check: (obj1, obj2, key) => !has(obj1, key) && has(obj2, key),
-    result: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}`,
+    toString: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}`,
   },
 ];
 
 const getPropertyAction = (obj1, obj2, key) => {
   const result = propertyActions.find(({ check }) => check(obj1, obj2, key));
   return result;
+};
+
+const toStringObjKeys = (objKeys, obj1, obj2) => {
+  const objArr = objKeys.reduce((acc, key) => {
+    const { toString } = getPropertyAction(obj1, obj2, key);
+    return [...acc, toString(obj1, obj2, key)];
+  }, []);
+  return `\n${objArr.join('\n')}`;
 };
 
 export default (file1, file2) => {
@@ -38,14 +46,8 @@ export default (file1, file2) => {
   const obj1Keys = Object.keys(obj1);
   const obj2Keys = Object.keys(obj2);
   const obj2FilteredKeys = obj2Keys.filter(key => !has(obj1, key));
-  const obj1Arr = obj1Keys.reduce((acc, key) => {
-    const { result } = getPropertyAction(obj1, obj2, key);
-    return [...acc, result(obj1, obj2, key)];
-  }, []);
-  const obj2Arr = obj2FilteredKeys.reduce((acc, key) => {
-    const { result } = getPropertyAction(obj1, obj2, key);
-    return [...acc, result(obj1, obj2, key)];
-  }, []);
-  console.log(`{\n${obj1Arr.concat(obj2Arr).join('\n')}\n}`);
-  return `{\n${obj1Arr.concat(obj2Arr).join('\n')}\n}`;
+  const obj1Arr = toStringObjKeys(obj1Keys, obj1, obj2);
+  const obj2Arr = toStringObjKeys(obj2FilteredKeys, obj1, obj2);
+  console.log(`{${obj1Arr.concat(obj2Arr)}\n}`);
+  return `{${obj1Arr.concat(obj2Arr)}\n}`;
 };
