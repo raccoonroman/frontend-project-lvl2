41a50606799b49972a8256f433aebf90ea5eafc7
Роман <kromanjs@gmail.com>
2019-10-31 23:51:51 +0200

    remove duplications

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,14 @@
 import gendiff from '../src';
 import result from '../__fixtures__/result';
 
-test('gendiff json', () => {
-  expect(gendiff('./__fixtures__/before.json', './__fixtures__/after.json')).toBe(result);
-  expect(gendiff(`${__dirname}/../__fixtures__/before.json`, `${__dirname}/../__fixtures__/after.json`)).toBe(result);
-});
 
-test('gendiff yaml', () => {
-  expect(gendiff('./__fixtures__/before.yml', './__fixtures__/after.yml')).toBe(result);
-  expect(gendiff(`${__dirname}/../__fixtures__/before.yml`, `${__dirname}/../__fixtures__/after.yml`)).toBe(result);
-});
+const formats = ['json', 'yml'];
+const relativePath = './__fixtures__/';
+const absolutePath = `${__dirname}/../__fixtures__/`;
+
+
+test.each(formats)('gendiff %s', (format) => {
+    expect(gendiff(`${relativePath}before.${format}`, `${relativePath}after.${format}`)).toBe(result);
+    expect(gendiff(`${absolutePath}before.${format}`, `${absolutePath}after.${format}`)).toBe(result);
+  },
+);
