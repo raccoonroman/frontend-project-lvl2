11ef4d9a04a1a6d93d7581f34fcbc26fb6bfc3a1
Роман <kromanjs@gmail.com>
2019-11-01 11:21:03 +0200

    remove duplications

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,11 @@
 import gendiff from '../src';
 import expected from '../__fixtures__/expected';
 
-
 const formats = ['json', 'yml'];
 const relativePath = './__fixtures__/';
 const absolutePath = `${__dirname}/../__fixtures__/`;
 
 const getResult = (pathType, format) => gendiff(`${pathType}before.${format}`, `${pathType}after.${format}`);
-
 
 test.each(formats)('gendiff %s', (format) => {
   expect(getResult(relativePath, format)).toBe(expected);
--- src/parsers.js
+++ src/parsers.js
@@ -2,16 +2,18 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
+const getContent = (file) => fs.readFileSync(`${file}`, 'utf8');
+
 const fileParsers = [
   {
     name: 'json',
     check: file => path.extname(file) === '.json',
-    parse: file => JSON.parse(fs.readFileSync(`${file}`, 'utf8')),
+    parse: file => JSON.parse(getContent(file)),
   },
   {
     name: 'yml',
     check: file => path.extname(file) === '.yml',
-    parse: file => yaml.safeLoad(fs.readFileSync(`${file}`, 'utf8')),
+    parse: file => yaml.safeLoad(getContent(file)),
   },
 ];
 
