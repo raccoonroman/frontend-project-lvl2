545d7f0ab12f151d5427bcce6d1eeba714cde236
Роман <kromanjs@gmail.com>
2020-01-27 21:35:25 +0200

    handle nested structure

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,23 @@
 import gendiff from '../src';
 import expected from '../__fixtures__/expected';
+import nestedExpected from '../__fixtures__/nested-expected';
 
 const formats = ['json', 'yml', 'ini'];
+// const formats = ['json'];
 const relativePath = './__fixtures__/';
 const absolutePath = `${__dirname}/../__fixtures__/`;
 
 const getResult = (pathType, format) => gendiff(`${pathType}before.${format}`, `${pathType}after.${format}`);
 
+const getNestedResult = (pathType, format) => gendiff(`${pathType}nested-before.${format}`, `${pathType}nested-after.${format}`);
+
+
 test.each(formats)('gendiff %s', (format) => {
   expect(getResult(relativePath, format)).toBe(expected);
   expect(getResult(absolutePath, format)).toBe(expected);
 });
+
+// test.each(formats)('gendiff nested %s', (format) => {
+//   expect(getNestedResult(relativePath, format)).toBe(nestedExpected);
+//   // expect(getNestedResult(absolutePath, format)).toBe(nestedExpected);
+// });
--- src/js/gendiff.js
+++ src/js/gendiff.js
@@ -1,50 +1,116 @@
-import { has } from 'lodash';
+import { has, uniqBy } from 'lodash';
+import fs from 'fs';
+import path from 'path';
 import parse from '../parsers';
+
+const areBothValuesObjects = (obj1, obj2, key) => {
+  const firstValueIsObject = obj1[key] instanceof Object;
+  const secondValueIsObject = obj2[key] instanceof Object;
+  return firstValueIsObject && secondValueIsObject;
+};
 
 const propertyActions = [
   {
-    name: 'unchanged',
-    check: (obj1, obj2, key) => has(obj1, key) && has(obj2, key) && obj1[key] === obj2[key],
-    toString: (obj1, obj2, key) => `    ${key}: ${obj1[key]}`,
+    state: 'unchanged',
+    check: (obj1, obj2, key) => {
+      const sameValues = has(obj1, key) && has(obj2, key) && obj1[key] === obj2[key];
+      return sameValues || areBothValuesObjects(obj1, obj2, key);
+    },
+    toString: (key, oldValue, newValue) => `    ${key}: ${newValue}`,
   },
   {
-    name: 'changedValue',
+    state: 'changed',
     check: (obj1, obj2, key) => has(obj1, key) && has(obj2, key) && obj1[key] !== obj2[key],
-    toString: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`,
+    toString: (key, oldValue, newValue) => `  + ${key}: ${newValue}\n  - ${key}: ${oldValue}`,
   },
   {
-    name: 'deletedValue',
+    state: 'deleted',
     check: (obj1, obj2, key) => has(obj1, key) && !has(obj2, key),
-    toString: (obj1, obj2, key) => `  - ${key}: ${obj1[key]}`,
+    toString: (key, oldValue) => `  - ${key}: ${oldValue}`,
   },
   {
-    name: 'addedValue',
+    state: 'added',
     check: (obj1, obj2, key) => !has(obj1, key) && has(obj2, key),
-    toString: (obj1, obj2, key) => `  + ${key}: ${obj2[key]}`,
+    toString: (key, oldValue, newValue) => `  + ${key}: ${newValue}`,
   },
 ];
 
-const getPropertyAction = (obj1, obj2, key) => {
-  const result = propertyActions.find(({ check }) => check(obj1, obj2, key));
+const getStateOfKey = (obj1, obj2, key) => {
+  const { state } = propertyActions.find(({ check }) => check(obj1, obj2, key));
+  return state;
+};
+
+const getToStringMethod = (currentState) => {
+  const { toString } = propertyActions.find(({ state }) => state === currentState);
+  return toString;
+};
+
+const getObjState = (obj1, obj2, key) => {
+  const state = getStateOfKey(obj1, obj2, key);
+  const result = { state, key };
+
+  if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+    result.oldValue = obj1[key];
+  }
+
+  if (Object.prototype.hasOwnProperty.call(obj2, key)) {
+    result.newValue = obj2[key];
+  }
+
   return result;
 };
 
-const toStringObjKeys = (objKeys, obj1, obj2) => {
-  const objArr = objKeys.reduce((acc, key) => {
-    const { toString } = getPropertyAction(obj1, obj2, key);
-    return [...acc, toString(obj1, obj2, key)];
-  }, []);
-  return `\n${objArr.join('\n')}`;
-};
-
-export default (file1, file2) => {
-  const obj1 = parse(file1);
-  const obj2 = parse(file2);
+const genDiffFromObjects = (obj1, obj2) => {
+  const keysWithChildren = [];
   const obj1Keys = Object.keys(obj1);
   const obj2Keys = Object.keys(obj2);
-  const obj2FilteredKeys = obj2Keys.filter(key => !has(obj1, key));
-  const obj1Arr = toStringObjKeys(obj1Keys, obj1, obj2);
-  const obj2Arr = toStringObjKeys(obj2FilteredKeys, obj1, obj2);
-  console.log(`{${obj1Arr.concat(obj2Arr)}\n}`);
-  return `{${obj1Arr.concat(obj2Arr)}\n}`;
+
+  const diffFromObject1 = obj1Keys.map((key) => {
+    const stateObj = getObjState(obj1, obj2, key);
+    const bothValuesObjects = areBothValuesObjects(obj1, obj2, key);
+
+    if (bothValuesObjects) {
+      keysWithChildren.push(key);
+    }
+
+    return stateObj;
+  });
+
+  const diffFromObject2 = obj2Keys.map(key => getObjState(obj1, obj2, key));
+
+  const result = uniqBy([...diffFromObject1, ...diffFromObject2], 'key');
+
+  keysWithChildren.forEach((it) => {
+    const obj = result.find(({ key }) => key === it);
+    delete obj.oldValue;
+    delete obj.newValue;
+    obj.children = genDiffFromObjects(obj1[it], obj2[it]);
+  });
+
+  return result;
 };
+
+const gendiff = (file1, file2) => {
+  const obj1 = parse(file1);
+  const obj2 = parse(file2);
+  const result = genDiffFromObjects(obj1, obj2);
+  return result;
+};
+
+const stringify = (file1, file2) => {
+  const diff = gendiff(file1, file2);
+
+  const jsonPath = path.join(__dirname, '..', '..', '__fixtures__', 'actual.json');
+  fs.writeFileSync(jsonPath, JSON.stringify(diff));
+
+  const result = diff.map((it) => {
+    const {
+      state, key, oldValue, newValue,
+    } = it;
+    const toString = getToStringMethod(state);
+    return toString(key, oldValue, newValue);
+  });
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default stringify;
